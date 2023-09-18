3d5fec987ce4be99d34a796f627d6520e41f8585
Polina Vyazankina <polinkavishinka@mail.ru>
2023-09-18 21:43:12 +0300

    update

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import ItemModel from "../models/ItemModel";
+
+class ShopItemClass extends React.Component {
+    
+    render() {
+        const { item } = this.props;
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">{item.descriptionFull}</div>
+                <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+ShopItemClass.propTypes = {
+    props: PropTypes.instanceOf(ItemModel)
+}
+
+export default ShopItemClass;
+
